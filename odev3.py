eb96033c404281f76de415fc991f1ef89fe93ec6
Hasanncany <hasancan.yildirim21@hotmail.com>
2023-03-29 16:58:25 +0300

    odev

--- odev3.py
+++ odev3.py
@@ -3,7 +3,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from time import sleep
 
-
 class Test():
     
     #Kullanıcı adı ve şifreyi boş test etme
